fe708ec44a026264719190a0822fdacc8715bda3
AndrewRMorgan <andrew.r.morgan@gmail.com>
2017-04-07 17:25:24 +1000

    delete months array, move variables, change w.Header to w.Write, and swap else code with if code

--- main.go
+++ main.go
@@ -11,46 +11,46 @@ import (
 )
 
 type Times struct {
-	Unix    int64  `json:"unix"`
-	Natural string `json:"natural"`
+	Unix    interface{} `json:"unix"`
+	Natural interface{} `json:"natural"`
 }
 
-var months = []string{"January", "February", "March", "April", "May", "June",
-	"July", "August", "September", "October", "November", "December"}
+var natural string
+var unix int64
 
 func Router(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path[1:] == "" || r.URL.Path[1:] == "favicon.ico" {
 		Start(w, r)
 	} else if r.URL.Path[1:] != "" {
-		//unix, natural := convert(r.URL.Path[1:])
-    var natural string
-    var unix int64
-
-    i, err := strconv.ParseInt(r.URL.Path[1:], 10, 64)
-
-  	if err != nil {
-  		n, _ := url.QueryUnescape(r.URL.Path[1:])
-  		n2, _, _ := fuzzytime.Extract(n)
-
-      if n2.ISOFormat() == "" {
-        times := Times{nil, nil}
-      } else {
-        unix, natural = naturalDate(n2)
-        times := Times{unix, natural}
-      }
-  	} else {
-      unix, natural = unixDate(i)
-      times := Times{unix, natural}
-  	}
-
+		var times = Times{}
+		i, err := strconv.ParseInt(r.URL.Path[1:], 10, 64)
+		if err == nil { // URL is an integer
+			unix = i
+			date := time.Unix(unix, 0)
+			natural := date.Format("January 2, 2006")
+			times = Times{Unix: unix, Natural: natural}
+		} else { // URL is not an integer
+			urlAfter, _ := url.QueryUnescape(r.URL.Path[1:])
+			str, _, _ := fuzzytime.Extract(urlAfter)
+			if str.ISOFormat() == "" { // String doesn't contain a date
+				times = Times{Unix: nil, Natural: nil}
+			} else { // String contains a date.
+				day := str.Day()
+				month := str.Month()
+				year := str.Year()
+				date := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
+				unix = date.Unix()
+				natural = date.Format("January 2, 2006")
+				times = Times{Unix: unix, Natural: natural}
+			}
+		}
 		js, err := json.Marshal(times)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-
 		w.Header().Set("Content-Type", "application/json")
-		w.Header(js)
+		w.Write(js)
 	}
 }
 
@@ -61,42 +61,4 @@ func main() {
 
 func Start(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Timestamp Microservice")
-}
-/*
-func convert(s string) (int64, string) {
-
-  var natural string
-  var unix int64
-
-  i, err := strconv.ParseInt(s, 10, 64)
-
-	if err != nil {
-		n, _ := url.QueryUnescape(s)
-		n2, _, _ := fuzzytime.Extract(n)
-
-    if n2.ISOFormat() == "" {
-      return nil, nil
-    }
-
-    unix, natural = natural(n2)
-    return unix, natural
-	} else {
-    unix, natural = unix(i)
-    return unix, natural
-	}
-}
-*/
-
-func naturalDate(s string) (int64, string) {
-  n, _ := time.Parse(time.RFC3339, s.ISOFormat())
-  unix = n.Unix()
-  natural = n.Format("January 2, 2006")
-  return unix, natural
-}
-
-func unixDate(i int64) (int64, string) {
-  unix = i
-  n := time.Unix(unix, 0)
-  natural = n.String()
-  return unix, natural
 }
