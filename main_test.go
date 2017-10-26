c157f22f8cff8af5183b1f8751da6ad917c5fec6
AndrewRMorgan <andrew.r.morgan@gmail.com>
2017-10-26 17:34:09 +0100

    add test tables

--- main_test.go
+++ main_test.go
@@ -1,26 +1,34 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestRouter(t *testing.T) {
-	expected := `{"unix":1450137600,"natural":"December 15, 2015"}`
+	tables := []struct {
+		sent     string
+		expected string
+	}{
+		{"1450137600", `{"unix":1450137600,"natural":"December 15, 2015"}`},
+		{"December%2015,%202015", `{"unix":1450137600,"natural":"December 15, 2015"}`},
+		{"January 27, 1988", `{"unix":570240000,"natural":"January 27, 1988"}`},
+	}
+	//expected := `{"unix":1450137600,"natural":"December 15, 2015"}`
 
-	req, err := http.NewRequest("GET", "/December%2015,%202015", nil)
-	if err != nil {
-		fmt.Println(err)
+	for _, table := range tables {
+		req, _ := http.NewRequest("GET", "/"+table.sent, nil)
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			Router(w, r)
+		})
+		handler.ServeHTTP(rr, req)
+		if rr.Body.String() != table.expected {
+			t.Errorf("The handler returned an unexpected body: Got %v but want %v",
+				rr.Body.String(), table.expected)
+		}
 	}
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		Router(w, r)
-	})
-	handler.ServeHTTP(rr, req)
-	if rr.Body.String() != expected {
-		t.Errorf("The handler returned an unexpected body: Got %v but want %v",
-			rr.Body.String(), expected)
-	}
+
 }
